3bdb897f3a7f8a7e20df0e1d5eefdcf32f3850dc
oliverPaloma <oliveip.j@gmail.com>
2024-12-02 22:27:22 -0300

    Add files via upload

--- ThermoPropCubicEos/cases/Isotermas/main.cpp
+++ ThermoPropCubicEos/cases/Isotermas/main.cpp
@@ -3,14 +3,19 @@
 
 int main ()
 {
-  auto T = 280.; //isoterma temperatura constante
- //auto P = 30000000.; obter
+  auto T = 280.; //isoterma temperatura constante 
+  //auto T = 300. // (Kelvin) for hexane and decane
+
+  //auto P = 30000000.; obter // P=100000. //(Pa) for hexane with decane
   
  // auto databasePath = "/home/fellipe/Dropbox/Pessoais/Downloads/ThermoPropCubicEos/database/test.yml";
 
   auto databasePath = "/home/palomajo/Documentos/ThermoPropCubicEos/database/test.yml";
-  auto components = "CO2"; //apenas um componete
+  //auto components = "CO2"; //apenas um componete
   //auto components = "CO2 H2O"; //apenas um componete
+  //testar com ('NC6,NC10') # Hexane/decane 
+
+  auto components = "NC6 NC10";
   std::vector<double> Tc,Pc,omega;
  
 
@@ -20,6 +25,8 @@ int main ()
 
   std::vector<double> z{1.}; //fração molar
   //std::vector<double> z{0.5,0.5}; //fração molar para mistura
+
+  //std::vector<double> z{0.2,0.8}; //fração molar para mistura  # Hexane/decane 
 
   read_database(Tc,Pc,omega,databasePath,components);
   
